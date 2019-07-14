890d3dc4b35d88dc5c8d6cb039c08d21997f39e8
Michael <mike.leibbrandt@gmail.com>
2019-07-14 17:31:33 +0200

    Structure and finished off lesson

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// New Context is created
+const MovieContext = React.createContext();
+
+// Provider Component
+class MovieProvider extends Component {
+  state = {
+    title: "DeadPool 2",
+    release: 2018,
+    cool: true
+  };
+  render() {
+    return (
+      <MovieContext.Provider
+        value={{
+          // We can build up an object that should be provided
+          movieTitle: this.state.title,
+          releaseYear: this.state.release,
+          setMovieTitle: title => this.setState({ title })
+        }}
+      >
+        {this.props.children}
+      </MovieContext.Provider>
+    );
+  }
+}
+
+// Breaking parent-child (prop-drilling) for this exercise
+const Cinema = () => <Movies />;
+
+class Movies extends Component {
+  render() {
+    return (
+      <div>
+        <MovieContext.Consumer>
+          {context => {
+            // Here Context is value prop in MovieContext.Provider, which is sending in
+            return (
+              <div>
+                Name: {context.movieTitle}
+                Year of release: {context.releaseYear}
+                <button onClick={() => context.setMovieTitle("BatMan")}>
+                  Change Title
+                </button>
+              </div>
+            );
+          }}
+        </MovieContext.Consumer>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <MovieProvider>
+        <div>
+          <Cinema />
+        </div>
+      </MovieProvider>
+    );
+  }
 }
 
 export default App;
